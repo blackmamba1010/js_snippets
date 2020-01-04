b580df59e87b861b65eb9d4560fd595a50148b02
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-01-04 14:58:57 +0700

    updated function.js

--- function.js
+++ function.js
@@ -4,11 +4,11 @@
 
 // functions is bread and butter in Javascript programming - Eloquent Javascript
 
-const square = function(x) {
-    return x*x;
-}
+// const square = function(x) {
+//     return x*x;
+// }
 
-console.log(square(2));
+// console.log(square(2));
 
 // binding and scope
 // var - is global and function scope
@@ -26,20 +26,44 @@ console.log(square(2));
 
 // nested scope
 
-const hummus = function(factor){
-    const ingredient = function(amount, unit, name){
-        let ingredientAmount = amount*factor;
-        if(ingredientAmount > 1){
-            unit += "s";
-        }
-        console.log(`${ingredientAmount} ${unit} ${name}`);
+// const hummus = function(factor){
+//     const ingredient = function(amount, unit, name){
+//         let ingredientAmount = amount*factor;
+//         if(ingredientAmount > 1){
+//             unit += "s";
+//         }
+//         console.log(`${ingredientAmount} ${unit} ${name}`);
+//     }
+//     ingredient(1, "can", "chickpeas");
+//     ingredient(0.25, "cup", "tahini");
+//     ingredient(0.25, "cup", "lemon juice");
+//     ingredient(1, "clove", "garlic");
+//     ingredient(2, "tablespoon", "olive oil");
+//     ingredient(0.5, "teaspoon", "cumin");
+// };
+
+// hummus(2);
+
+
+// arrow function
+
+const power = (base, exponent) => {
+    let result = 1;
+    for(let count = 0; count < exponent; count++){
+        result *= base;
     }
-    ingredient(1, "can", "chickpeas");
-    ingredient(0.25, "cup", "tahini");
-    ingredient(0.25, "cup", "lemon juice");
-    ingredient(1, "clove", "garlic");
-    ingredient(2, "tablespoon", "olive oil");
-    ingredient(0.5, "teaspoon", "cumin");
+    return result;
 };
 
-hummus(2);
+console.log(power(2,3)); // 8
+
+// call stack
+
+function chicken(){
+    return egg();
+}
+function egg(){
+    return chicken();
+}
+
+console.log(chicken() + "came first"); // RangeError: maximum call stack size exceeded
