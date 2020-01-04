88677367df478d7b565f2515171f1342537f0bd0
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-01-04 14:30:07 +0700

    first commit

---
+++ function.js
@@ -0,0 +1,45 @@
+// all about function in javscript
+// follow Eloquent Javascript book of Marijn Haverbeke
+
+
+// functions is bread and butter in Javascript programming - Eloquent Javascript
+
+const square = function(x) {
+    return x*x;
+}
+
+console.log(square(2));
+
+// binding and scope
+// var - is global and function scope
+// let - is function and block scope
+// const - is function and block scope but this is not changable
+
+// const x = 10;
+// if(true){
+//     let y = 10;
+//     var z = 20;
+// }
+// x = 30; //error here
+// console.log(y) // error here
+// console.log(z) // 20 
+
+// nested scope
+
+const hummus = function(factor){
+    const ingredient = function(amount, unit, name){
+        let ingredientAmount = amount*factor;
+        if(ingredientAmount > 1){
+            unit += "s";
+        }
+        console.log(`${ingredientAmount} ${unit} ${name}`);
+    }
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini");
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin");
+};
+
+hummus(2);
